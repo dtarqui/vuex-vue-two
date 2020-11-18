8730e2bedaac78773cf212bfc9f521b4abe6cf35
Fulano2141 <macuto2142dt@gmail.com>
2020-11-17 22:05:17 -0400

    without vuex

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <Result :counter="counter"></Result>
+        <hr />
+        <Counter @updated="counter += $event"></Counter>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter.vue";
+import Result from "./components/Result.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data: () => ({
+    counter: 0,
+  }),
   components: {
-    HelloWorld
-  }
-}
+    Counter,
+    Result,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Result.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Counter is: {{ counter }}</p>
+</template>
+
+<script>
+export default {
+  props: ["counter"],
+};
+</script>
+
+<style>
+</style>
