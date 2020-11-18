3ebc363f4b45ab6376d8fadd4512fde63037a10c
Fulano2141 <macuto2142dt@gmail.com>
2020-11-17 22:38:01 -0400

    using mapGetters

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <p>Counter is: {{ counter }}</p>
-    <p>Clicks is: {{ clicks }}</p>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Clicks is: {{ stringCounter }}</p>
   </div>
 </template>
 
 <script>
-// import { mapGetters } from "vuex";
+import { mapGetters } from "vuex";
 export default {
-  computed: {
-    counter() {
-      return this.$store.getters.doubleCounter;
-    },
-    clicks() {
-      return this.$store.getters.stringCounter;
-    },
-  },
+  computed: mapGetters(["doubleCounter", "stringCounter"]),
 };
 </script>
 
